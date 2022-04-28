c9d195f0b30fea2dcc52e1040dec62b9ba4cdfcc
Minh <quachquangminh@gmail.com>
2022-04-28 14:33:58 +0700

    clear terminal screen and add update info

--- task_manager/src/main.rs
+++ task_manager/src/main.rs
@@ -1,40 +1,55 @@
+use std::{thread, time};
+
+// let ten_millis = time::Duration::from_millis(10);
+// let now = time::Instant::now();
+
+// thread::sleep(ten_millis);
+
+// assert!(now.elapsed() >= ten_millis);
+
 fn main() {
-    // clearscreen::clear().unwrap();
-    println!("Cpu");
+    loop {
+        let time = time::Duration::from_secs(2);
+        clearscreen::clear().unwrap();
+        task_manager();
+        thread::sleep(time);
+    }
+}
+
+fn task_manager() {
+    println!("----------Cpu");
     let cpu_speed = sys_info::cpu_speed();
     if let Ok(speed) = cpu_speed {
         println!("speed: {} MHz", speed);
     }
 
-    println!("\nRam");
+    println!("\n----------Ram");
     let memory_info = sys_info::mem_info();
     if let Ok(info) = memory_info {
         println!("total: {}G", kb_to_gb(info.total));
         println!("avail: {}G", kb_to_gb(info.avail));
-        println!("used: {}G", kb_to_gb(info.total - info.avail));
+        println!("used:  {}G", kb_to_gb(info.total - info.avail));
     }
 
-    println!("\nDisk");
+    println!("\n----------Disk");
     let disk_info = sys_info::disk_info();
     if let Ok(info) = disk_info {
         println!("total: {}G", kb_to_gb(info.total));
-        println!("free: {}G", kb_to_gb(info.free));
-        println!("used: {}G", kb_to_gb(info.total - info.free));
+        println!("free:  {}G", kb_to_gb(info.free));
+        println!("used:  {}G", kb_to_gb(info.total - info.free));
     }
 
-    println!("\nOS info");
+    println!("\n----------OS info");
     if let Ok(type_os) = sys_info::os_type() {
         println!("Operating System: {}", type_os);
     }
     if let Ok(release_os) = sys_info::os_release() {
-        println!("Release: {}", release_os);
+        println!("Release:          {}", release_os);
     }
 
     if let Ok(host_name) = sys_info::hostname() {
-        print!("Host: {}", host_name);
+        print!("Host:               {}\n", host_name);
     }
-
-    println!();
 
 }
 
