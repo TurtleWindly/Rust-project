43fc9452e4e00e6666beaf420409e41b94d18e59
minh <quachquangminh2006@gmail.com>
2022-11-29 11:29:32 +0700

    ron reader and hash pass and checking complete

--- login_machine/src/main.rs
+++ login_machine/src/main.rs
@@ -1,28 +1,33 @@
-use std::{collections::HashMap, fs::File};
+use std::fs::File;
 
+// Get input
+use std::io;
+use std::io::Write;
+
+// Hashing part
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+// ron
 use ron::de::from_reader;
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
-struct Config {
-    boolean: bool,
-    float: f32,
-    map: HashMap<u8, char>,
-    nested: Nested,
-    tuple: (u32, u32),
-    vec: Vec<Nested>,
+#[derive(Hash, Deserialize)]
+struct User {
+    name: String,
+    pass: u64,
 }
 
-#[derive(Debug, Deserialize)]
-struct Nested {
-    a: String,
-    b: char,
+#[derive(Deserialize)]
+struct List {
+    user_list: Vec<User>,
 }
 
 fn main() {
+    // Get data from user.ron
     let input_path = String::from("src/user.ron");
     let f = File::open(&input_path).expect("Failed opening file");
-    let config: Config = match from_reader(f) {
+    let list: List = match from_reader(f) {
         Ok(x) => x,
         Err(e) => {
             println!("Failed to load config: {}", e);
@@ -31,7 +36,79 @@ fn main() {
         }
     };
 
-    println!("Config: {:?}", &config);
+    let login_name = name_login();
+    // Check login name
+    check_name(&list, login_name);
+
+    let login_pass = pass_login();
+    // hah the pass
+    let hashed_pass = calculate_hash(&login_pass);
+    // Check the password
+    check_pass(&list, hashed_pass);
+
+    println!("Corract you have login successfully !");
+
 }
 
-// TLDG: Need a struct to hold all of ron data
+fn name_login() -> String {
+    let mut name = String::new();
+    println!("---------------------------------------------------------");
+    print!("login: ");
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Can't read input");
+    // remove \n
+    name.pop();
+    // then return name
+    name
+}
+
+fn pass_login() -> String {
+    let mut pass = String::new();
+    print!("Password: ");
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut pass)
+        .expect("Can't read input");
+    // remove \n
+    pass.pop();
+    // then return pass
+    pass
+}
+
+fn check_name(list: &List, name: String) {
+    let mut name_exsit = false;
+    for user in &list.user_list {
+        if name == user.name {
+            name_exsit = true;
+        }
+    }
+
+    // if name not exsit then quit
+    if !name_exsit {
+        println!("Name do not exsit");
+        std::process::exit(0);
+    }
+}
+
+fn check_pass(list: &List, hashed_pass: u64) {
+    let mut correct_pass = false;
+    for user in &list.user_list {
+        if hashed_pass == user.pass {
+            correct_pass = true;
+        }
+    }
+
+    // if wrong password then exit
+    if !correct_pass {
+        println!("Wrong password");
+        std::process::exit(0);
+    }
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
