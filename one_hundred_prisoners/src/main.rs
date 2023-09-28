56f5984edba2e70c2015499b345ce982a65932b2
TurtleWindy <quachquangminh2006@gmail.com>
2023-09-28 14:17:33 +0700

    added strategy for prison and completed

--- one_hundred_prisoners/src/main.rs
+++ one_hundred_prisoners/src/main.rs
@@ -9,10 +9,15 @@ fn gen_tmp_choices() -> Vec<usize> {
     return  tmp_choices;
 }
 
-fn prison_game() -> bool {
+fn gen_cupboard() -> Vec<i32> {
     let mut rng = rand::thread_rng();
     let mut cupboard: Vec<i32> = (1..=100).collect();
     cupboard.shuffle(&mut rng);
+    return  cupboard;
+}
+
+fn prison_game() -> bool {
+    let cupboard = gen_cupboard();
 
     let mut free_prisoners = 0;
 
@@ -29,13 +34,47 @@ fn prison_game() -> bool {
     return free_prisoners == 100;
 }
 
+fn prison_game_strategy() -> bool {
+    let cupboard = gen_cupboard();
+
+    let mut free_prisoners = 0;
+
+    for prisoners in 1..=100 {
+        let mut tmp_choice = prisoners;
+        for _drawer in 0..50 {
+            if prisoners == cupboard[tmp_choice as usize - 1] {
+                free_prisoners += 1;
+                break;
+            } else {
+                tmp_choice = cupboard[tmp_choice as usize - 1];
+            }
+        }
+    }
+
+    return free_prisoners == 100;
+}
+
 fn main() {
     let game_play = 100_000.0;
-    let mut game_won = 0.0;
+    let mut game_won = 0;
+
+    // Without strategy
+    println!("Prison game without strategy");
     for _game in 0..game_play as usize {
-        if prison_game() { game_won += 1.0 }
+        if prison_game() { game_won += 1 }
     }
     println!("Game play: {}", game_play as i32);
-    println!("Game won: {}", game_won as i32);
-    println!("Winrate: {}", game_won / game_play);
+    println!("Game won: {}", game_won);
+    println!("Winrate: {}", game_won as f32 / game_play);
+
+    // With strategy
+    println!("Prison game with strategy");
+    game_won = 0;
+
+    for _game in 0..game_play as usize {
+        if prison_game_strategy() { game_won += 1 }
+    }
+    println!("Game play: {}", game_play as i32);
+    println!("Game won: {}", game_won);
+    println!("Winrate: {}", game_won as f32 / game_play);
 }
