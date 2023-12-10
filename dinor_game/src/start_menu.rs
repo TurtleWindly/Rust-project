f2e9f3dbe9c54161050d5d937bab5be6f7b6a4e5
TurtleWindy <quachquangminh2006@gmail.com>
2023-12-10 22:44:04 +0700

    adding logo to game

--- dinor_game/src/start_menu.rs
+++ dinor_game/src/start_menu.rs
@@ -15,35 +15,59 @@ impl Plugin for StartMenuPlugin {
 #[derive(Component)]
 struct Rectangle;
 
-fn setup_start_animation(mut commands: Commands, windows: Query<&Window>) {
+fn setup_start_animation(
+    mut commands: Commands,
+    windows: Query<&Window>,
+    asset_server: Res<AssetServer>,
+) {
     let window = windows.single();
-    commands.spawn((
-        Rectangle,
-        Name::new("Rectangle Animation"),
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::rgb(1., 1., 1.),
-                custom_size: Some(Vec2::new(window.width(), window.height())),
-                ..default()
-            },
-            transform: Transform {
-                translation: Vec3 {
-                    x: window.width() / 3.,
-                    z: 99.,
+    commands
+        .spawn((
+            Rectangle,
+            Name::new("Rectangle Animation"),
+            SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(1., 1., 1.),
+                    custom_size: Some(Vec2::new(window.width(), window.height())),
+                    ..default()
+                },
+                transform: Transform {
+                    translation: Vec3 {
+                        x: window.width() / 3.,
+                        z: 99.,
+                        ..default()
+                    },
                     ..default()
                 },
                 ..default()
             },
-            ..default()
-        },
-    ));
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                Name::new("Logo"),
+                SpriteBundle {
+                    texture: asset_server.load("Logo.png"),
+                    transform: Transform {
+                        translation: Vec3 {
+                            x: -180.,
+                            ..default()
+                        },
+                        ..default()
+                    },
+                    ..default()
+                },
+            ));
+        });
 }
 
-fn start_animation(mut commands: Commands, mut query: Query<(&mut Transform, Entity), With<Rectangle>>) {
+fn start_animation(
+    mut commands: Commands,
+    mut query: Query<(&mut Transform, Entity), With<Rectangle>>,
+) {
     if let Ok((mut transform, entity)) = query.get_single_mut() {
         transform.translation.x += 7.;
         if transform.translation.x > 1500. {
-            commands.entity(entity).despawn();
+            commands.entity(entity).despawn_recursive();
         }
     }
 }
