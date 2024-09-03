a76a2819aa1098289f061a01d008a18185298464
TurtleWindy <quachquangminh2006@gmail.com>
2024-09-03 19:43:07 +0700

    working game i guess

--- ldtk_game/src/goal.rs
+++ ldtk_game/src/goal.rs
@@ -1,16 +1,43 @@
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 
+use crate::player::Player;
+
 pub struct GoalPlugin;
 
 impl Plugin for GoalPlugin {
     fn build(&self, app: &mut App) {
-        app;
+        app.register_ldtk_entity::<GoalBundle>("Goal")
+            .add_systems(Update, check_goal);
     }
 }
 
+#[derive(Default, Component)]
+struct Goal;
+
 #[derive(Default, Bundle, LdtkEntity)]
 struct GoalBundle {
+    goal: Goal,
     #[sprite_sheet_bundle]
     sprite_sheet_bundle: LdtkSpriteSheetBundle,
+    #[grid_coords]
+    grid_coords: GridCoords,
+}
+
+fn check_goal(
+    level_selection: ResMut<LevelSelection>,
+    players: Query<&GridCoords, (With<Player>, Changed<GridCoords>)>,
+    goals: Query<&GridCoords, With<Goal>>,
+) {
+    if players
+        .iter()
+        .zip(goals.iter())
+        .any(|(player_grid_coords, goal_grid_coords)| player_grid_coords == goal_grid_coords)
+    {
+        let indices = match level_selection.into_inner() {
+            LevelSelection::Indices(indices) => indices,
+            _ => panic!("Level go brrr"),
+        };
+        indices.level += 1;
+    }
 }
--- ldtk_game/src/player.rs
+++ ldtk_game/src/player.rs
@@ -5,12 +5,56 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app;
+        app.register_ldtk_entity::<PlayerBundle>("Player")
+            .add_systems(
+                Update,
+                (move_player_from_input, translate_grid_coords_entities),
+            );
     }
 }
 
+const GRID_SIZE: i32 = 16;
+
+#[derive(Default, Component)]
+pub struct Player;
+
 #[derive(Default, Bundle, LdtkEntity)]
 struct PlayerBundle {
+    player: Player,
     #[sprite_sheet_bundle]
     sprite_sheet_bundle: LdtkSpriteSheetBundle,
+    #[grid_coords]
+    grid_coords: GridCoords,
+}
+
+fn move_player_from_input(
+    mut players: Query<&mut GridCoords, With<Player>>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    let movement_direction = if input.just_pressed(KeyCode::KeyW) {
+        GridCoords::new(0, 1)
+    } else if input.just_pressed(KeyCode::KeyA) {
+        GridCoords::new(-1, 0)
+    } else if input.just_pressed(KeyCode::KeyS) {
+        GridCoords::new(0, -1)
+    } else if input.just_pressed(KeyCode::KeyD) {
+        GridCoords::new(1, 0)
+    } else {
+        return;
+    };
+
+    for mut player_grid_coords in players.iter_mut() {
+        let destination = *player_grid_coords + movement_direction;
+        *player_grid_coords = destination;
+    }
+}
+
+fn translate_grid_coords_entities(
+    mut grid_coords_entities: Query<(&mut Transform, &GridCoords), Changed<GridCoords>>,
+) {
+    for (mut transform, grid_coords) in grid_coords_entities.iter_mut() {
+        transform.translation =
+            bevy_ecs_ldtk::utils::grid_coords_to_translation(*grid_coords, IVec2::splat(GRID_SIZE))
+                .extend(transform.translation.z);
+    }
 }
