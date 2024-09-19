c1b866a4c3e8cd31d152249fc8f22add00cf5aa5
TurtleWindy <quachquangminh2006@gmail.com>
2024-09-19 09:37:57 +0700

    turn on and off Tourch

--- ldtk_game/src/tourch.rs
+++ ldtk_game/src/tourch.rs
@@ -2,18 +2,31 @@ use avian2d::prelude::*;
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 use bevy_light_2d::prelude::*;
+use bevy_mod_picking::prelude::*;
 
 pub struct TourchPlugin;
+
+const DEFAULT_INTENSITY: f32 = 3.0;
+
+#[derive(Default, Component)]
+struct Tourch;
+
+#[derive(Event)]
+struct TourchClicked(Entity);
+
+impl From<ListenerInput<Pointer<Down>>> for TourchClicked {
+    fn from(event: ListenerInput<Pointer<Down>>) -> Self {
+        TourchClicked(event.target)
+    }
+}
 
 impl Plugin for TourchPlugin {
     fn build(&self, app: &mut App) {
         app.register_ldtk_entity::<TourchBundle>("Tourch")
-            .add_systems(Update, setup);
+            .add_event::<TourchClicked>()
+            .add_systems(Update, (setup, receive_clicked));
     }
 }
-
-#[derive(Default, Component)]
-struct Tourch;
 
 #[derive(Default, Bundle, LdtkEntity)]
 struct TourchBundle {
@@ -29,11 +42,27 @@ fn setup(mut commands: Commands, tourches: Query<Entity, Added<Tourch>>) {
         commands.entity(tourch).insert((
             PointLight2d {
                 radius: 100.0,
-                intensity: 3.0,
+                intensity: DEFAULT_INTENSITY,
                 ..default()
             },
             Collider::rectangle(16.0, 16.0),
             RigidBody::Static,
+            On::<Pointer<Down>>::send_event::<TourchClicked>(),
         ));
+    }
+}
+
+fn receive_clicked(
+    mut tourch_clicked: EventReader<TourchClicked>,
+    mut query: Query<&mut PointLight2d>,
+) {
+    for event in tourch_clicked.read() {
+        if let Ok(mut tourch) = query.get_mut(event.0) {
+            if tourch.intensity == 0.0 {
+                tourch.intensity = DEFAULT_INTENSITY;
+            } else {
+                tourch.intensity = 0.0;
+            }
+        }
     }
 }
