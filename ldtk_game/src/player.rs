f47c25d621a54b2e5e3fd978404dc794b1f405f7
TurtleWindy <quachquangminh2006@gmail.com>
2024-09-23 19:25:25 +0700

    adding leafwing_input_manager to player handler

--- ldtk_game/src/player.rs
+++ ldtk_game/src/player.rs
@@ -3,12 +3,14 @@ use avian2d::prelude::*;
 use bevy::core_pipeline::bloom::BloomSettings;
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
+use leafwing_input_manager::prelude::*;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.register_ldtk_entity::<PlayerBundle>("Player")
+            .add_plugins(InputManagerPlugin::<PlayerAction>::default())
             .add_systems(Startup, setup_camera)
             .add_systems(
                 Update,
@@ -25,6 +27,14 @@ impl Plugin for PlayerPlugin {
 
 const GRID_SIZE: i32 = 16;
 const CAM_LERP_FACTOR: f32 = 2.0;
+
+#[derive(Actionlike, PartialEq, Eq, Hash, Clone, Copy, Debug, Reflect)]
+enum PlayerAction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
 
 #[derive(Default, Component)]
 pub struct Player;
@@ -55,38 +65,54 @@ struct PlayerBundle {
     movement_speed: MovementSpeed,
 }
 
+impl PlayerBundle {
+    fn default_input_map() -> InputMap<PlayerAction> {
+        use PlayerAction::*;
+        let mut input_map = InputMap::default();
+
+        // Movement
+        input_map.insert(Up, KeyCode::KeyW);
+        input_map.insert(Down, KeyCode::KeyS);
+        input_map.insert(Left, KeyCode::KeyA);
+        input_map.insert(Right, KeyCode::KeyD);
+
+        input_map
+    }
+}
+
 fn setup_player(mut commands: Commands, players: Query<Entity, Added<Player>>) {
     for player in &players {
         commands.entity(player).insert((
             RigidBody::Dynamic,
             Collider::rectangle(16.0, 32.0),
             LockedAxes::ROTATION_LOCKED,
+            InputManagerBundle::with_map(PlayerBundle::default_input_map()),
         ));
     }
 }
 
 fn move_player_from_input(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut players: Query<(&MovementSpeed, &mut LinearVelocity), With<Player>>,
+    // keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut players: Query<
+        (
+            &MovementSpeed,
+            &mut LinearVelocity,
+            &ActionState<PlayerAction>,
+        ),
+        With<Player>,
+    >,
 ) {
-    for (movement_speed, mut linear_velocity) in &mut players {
-        let left = keyboard_input.any_pressed([KeyCode::KeyA, KeyCode::ArrowLeft]);
-        let right = keyboard_input.any_pressed([KeyCode::KeyD, KeyCode::ArrowRight]);
-        let up = keyboard_input.any_pressed([KeyCode::KeyW, KeyCode::ArrowUp]);
-        let down = keyboard_input.any_pressed([KeyCode::KeyS, KeyCode::ArrowDown]);
+    for (movement_speed, mut linear_velocity, action_state) in &mut players {
+        let up = action_state.pressed(&PlayerAction::Up);
+        let down = action_state.pressed(&PlayerAction::Down);
+        let left = action_state.pressed(&PlayerAction::Left);
+        let right = action_state.pressed(&PlayerAction::Right);
         let horizontal = right as i8 - left as i8;
         let vertical = up as i8 - down as i8;
         // TODO: WTF
         linear_velocity.x = horizontal as Scalar * movement_speed.0;
         linear_velocity.y = vertical as Scalar * movement_speed.0;
     }
-    // } else if input.just_pressed(KeyCode::KeyA) {
-    //     GridCoords::new(-1, 0)
-
-    // for (mut player_grid_coords, linear_velocity) in players.iter_mut() {
-    //     let destination = *player_grid_coords + movement_direction;
-    //     *player_grid_coords = destination;
-    // }
 }
 
 fn translate_grid_coords_entities(
